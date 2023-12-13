bd93d85fd4ff7b23d5e9435acda227d164376022
michalgalant00 <s95401@pollub.edu.pl>
2023-12-13 12:38:13 +0100

    poprawione generowanie podsumowań

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 import tkinter as tk
 from tkinter import ttk
 import numpy as np
+from fractions import Fraction
 
 class AHPApp:
     def __init__(self, master):
@@ -15,6 +16,8 @@ class AHPApp:
 
         # Kryteria i alternatywy
         self.criteria = ["Spalanie", "Cena", "Stan mechaniczny", "Stan lakierniczy", "Wyposażenie"]
+        # tmp do debugowania
+        # self.criteria = ["kryt 1", "kryt 2", "kryt 3"]
         self.slider_values = [1/9, 1/7, 1/5, 1/3, 1, 3, 5, 7, 9]
 
         # Macierz porównań par kryteriów
@@ -25,6 +28,21 @@ class AHPApp:
 
         # Aktualne porównanie kryteriów
         self.current_comparison = 0
+
+        # Aktualne porównanie kryteriów
+        self.current_i = None
+        self.current_j = None
+
+        # Czas ostatniej udzielonej odpowiedzi
+        self.last_answer_time = datetime.now()  # Initialize with the current time
+
+        # Katalog, w którym będą przechowywane podsumowania
+        self.summary_dir = "podsumowania"
+        # Tekst podsumowujący zmiany w odpowiedziach
+        self.summary_text = ""
+
+        # Lista przechowująca odpowiedzi użytkownika
+        self.user_responses = []
 
         # GUI
         self.create_welcome_gui()
@@ -54,6 +72,13 @@ class AHPApp:
     def create_comparison_gui(self):
         def on_slider_change(value):
             print(f"Wartość suwaka: {self.slider_values[int(value)]}")
+            
+            # Dodaj informację o zmianie odpowiedzi do podsumowania
+            elapsed_time = datetime.now() - self.last_answer_time
+            elapsed_seconds = elapsed_time.total_seconds()
+            selected_value = self.slider_values[int(value)]
+            self.summary_text += f"minelo sekund: {elapsed_seconds} pobrana wartosc: {selected_value}\n"
+
         # Usuń ekran powitalny
         self.welcome_frame.destroy()
 
@@ -65,10 +90,10 @@ class AHPApp:
         form_frame.grid(row=0, column=0, columnspan=3)
 
         # Porównanie aktualnych kryteriów
-        i, j = self.get_next_comparison_pair()
-        if i is not None:
-            criterion = self.criteria[i]
-            col_criterion = self.criteria[j]
+        self.current_i, self.current_j = self.get_next_comparison_pair()
+        if self.current_i is not None:
+            criterion = self.criteria[self.current_i]
+            col_criterion = self.criteria[self.current_j]
 
             # Zeruj zawartość okna
             for widget in form_frame.winfo_children():
@@ -88,10 +113,10 @@ class AHPApp:
                     scale_var.set(round(float(x)))).grid(row=1, column=1)
 
             ttk.Label(form_frame, text=f"{col_criterion}").grid(row=1, column=2, padx=(0, 5))
-            self.scale_vars[i][j] = scale_var  # Przypisanie DoubleVar do listy
+            self.scale_vars[self.current_i][self.current_j] = scale_var  # Przypisanie DoubleVar do listy
 
-            # Przycisk do przejścia do kolejnego porównania
-            ttk.Button(self.master, text="Przejdź dalej", command=self.show_next_comparison).grid(row=2, column=0, columnspan=3, pady=(10, 0))
+            # Przycisk do zatwierdzenia odpowiedzi
+            ttk.Button(self.master, text="Zatwierdź odpowiedź", command=self.confirm_answer).grid(row=3, column=0, columnspan=3, pady=(10, 0))
 
             # Zwiększenie licznika porównań
             self.current_comparison += 1
@@ -99,6 +124,25 @@ class AHPApp:
             # Jeśli nie ma więcej par do porównania, przejdź do podsumowania
             self.show_summary()
 
+    def confirm_answer(self):
+        # Dodaj informację o ostatecznej odpowiedzi do podsumowania
+        selected_value = self.slider_values[int(self.scale_vars[self.current_i][self.current_j].get())]
+        self.summary_text += f"ostateczna odpowiedz: {selected_value}\n"
+
+        # Dodaj informacje o porównaniu do listy
+        elapsed_time = datetime.now() - self.last_answer_time
+        elapsed_seconds = elapsed_time.total_seconds()
+        self.user_responses.append({
+            'pytanie': f"{self.criteria[self.current_i]} vs {self.criteria[self.current_j]}",
+            'czas': elapsed_seconds,
+            'odpowiedz': selected_value
+        })
+
+        # Zaktualizuj czas ostatniej udzielonej odpowiedzi
+        self.last_answer_time = datetime.now()
+
+        # Przejdź do kolejnego porównania lub podsumowania
+        self.show_next_comparison()
 
     def get_next_comparison_pair(self):
         for i in range(len(self.criteria)):
@@ -113,58 +157,64 @@ class AHPApp:
 
         # Utwórz ramkę podsumowania
         summary_root = tk.Tk()
-        summary_root.title("Podsumowanie wyników")
+        summary_root.title("Podsumowanie ankiety")
 
         result_frame = ttk.Frame(summary_root, padding="10")
         result_frame.grid(row=0, column=0)
 
-        ttk.Label(result_frame, text="Wagi").grid(row=0, column=0, columnspan=2)
+        # Label PODSUMOWANIE ANKIETY
+        ttk.Label(result_frame, text="PODSUMOWANIE ANKIETY").grid(row=0, column=0, columnspan=2)
 
-        result_text = tk.Text(result_frame, height=len(self.criteria), width=20, state="disabled")
-        result_text.grid(row=1, column=0, columnspan=2)
+        # Wprowadź podsumowanie do pola TextArea
+        summary_text = tk.Text(result_frame, height=20, width=40)
+        summary_text.grid(row=1, column=0, columnspan=2)
 
-        # Oblicz wagi i wyświetl wyniki
-        self.calculate_weights()
-        weights = ahp(self.criteria_matrix)
-        result_str = ""
-
+        # Wagi
+        summary_text.insert(tk.END, "WAGI:\n")
+        weights = self.calculate_weights()  # Wywołaj funkcję do obliczania wag
         for i, weight in enumerate(weights):
-            result_str += f"{self.criteria[i]}: {weight:.3f}\n"
+            summary_text.insert(tk.END, f"{self.criteria[i]}: {weight:.3f}\n")
 
-        # Zapisz wyniki do pliku
-        self.save_results(result_str)
+        # Odpowiedzi
+        summary_text.insert(tk.END, "\nODPOWIEDZI:\n")
+        for idx, response in enumerate(self.user_responses):
+            czas = round(response['czas'], 2)
+            odpowiedz = Fraction(response['odpowiedz']).limit_denominator() if '.' in str(response['odpowiedz']) else response['odpowiedz']
+            summary_text.insert(tk.END, f"{idx+1}. {response['pytanie']}\n")
+            summary_text.insert(tk.END, f"Czas: {czas} sekund, Odpowiedź: {odpowiedz}\n")
 
-        result_text.config(state="normal")
-        result_text.insert(tk.END, result_str)
-        result_text.config(state="disabled")
+        summary_text.config(state="disabled")
 
         summary_root.mainloop()
 
+
     def save_results(self, result_str):
         # Utwórz katalog PODSUMOWANIA, jeśli nie istnieje
-        summary_dir = "podsumowania"
-        if not os.path.exists(summary_dir):
-            os.makedirs(summary_dir)
+        if not os.path.exists(self.summary_dir):
+            os.makedirs(self.summary_dir)
 
         # Utwórz nazwę pliku zgodnie z wymaganiami
         now = datetime.now()
         date_str = now.strftime("%d-%m-%Y")
         time_str = now.strftime("%H-%M")
         filename = f"podsumowanie_{date_str}_{time_str}.txt"
-        filepath = os.path.join(summary_dir, filename)
+        filepath = os.path.join(self.summary_dir, filename)
 
         # Zapisz wyniki do pliku
         with open(filepath, "w") as file:
             file.write(result_str)
 
     def calculate_weights(self):
+        matrix = np.ones((len(self.criteria), len(self.criteria)), dtype=float)
         for i in range(len(self.criteria)):
             for j in range(len(self.criteria)):
                 if i == j:
                     continue
                 scale_var = self.scale_vars[i][j]
                 if scale_var is not None:
-                    self.criteria_matrix[i, j] = scale_var.get()
+                    matrix[i, j] = scale_var.get()
+        # Oblicz wagi za pomocą algorytmu AHP
+        return ahp(matrix)
 
 def ahp(matrix):
     col_sums = matrix.sum(axis=0)
